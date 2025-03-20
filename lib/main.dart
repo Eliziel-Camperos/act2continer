f2c9f7267c6b94526459354305a139395d8321b3
ELIZIEL_CAMPEROS_GARCIA <a.22308051281159@cbtis128.edu.mx>
2025-03-20 18:56:15 +0000

    no

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,48 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp()); // Ejecuta la aplicación Flutter
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false, // Oculta la marca de depuración
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+              'Eliziel Camperos García'), // Título de la barra de navegación
+          backgroundColor:
+              Colors.blue, // Color azul para la barra de navegación
+        ),
+        body: Center(
+          child: Container(
+            width: 300, // Ancho del contenedor
+            height: 100, // Altura del contenedor
+            color: Colors.blue, // Color de fondo azul
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment
+                  .center, // Centra los elementos verticalmente
+              children: [
+                Text(
+                  'Dato 1', // Primer dato
+                  style: TextStyle(
+                      color: Colors.white, fontSize: 18), // Estilo del texto
+                ),
+                Text(
+                  'Dato 2', // Segundo dato
+                  style: TextStyle(
+                      color: Colors.white, fontSize: 18), // Estilo del texto
+                ),
+                Text(
+                  'Dato 3', // Tercer dato
+                  style: TextStyle(
+                      color: Colors.white, fontSize: 18), // Estilo del texto
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
